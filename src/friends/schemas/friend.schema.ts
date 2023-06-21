d2da4740c7c670a58aaa3dc7c224330e1b9c0447
Mateusz Pilch <skapuni@gmail.com>
2023-06-21 22:32:29 +0200

    Nest js updated 9,0 -> 10.0

--- src/friends/schemas/friend.schema.ts
+++ src/friends/schemas/friend.schema.ts
@@ -1,8 +1,8 @@
 import { Prop, Schema} from '@nestjs/mongoose';
-import { Document, ObjectId, Types } from 'mongoose';
+import { ObjectId, Types } from 'mongoose';
 
 @Schema()
-export class Friend extends Document{
+export class Friend{
   
   @Prop({unique: true, type: Types.ObjectId})
   _id: ObjectId;
